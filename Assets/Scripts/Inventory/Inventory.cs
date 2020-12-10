39030c09b9f4d5e5c74325d62ef4516642906d7f
michelleisibach <50678154+michelleisibach@users.noreply.github.com>
2020-12-10 12:43:21 +0100

    Selected Weapon are appearing on the Screen

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -18,6 +18,21 @@ public class Inventory : MonoBehaviour
     public GameObject WeaponSelector;
     public GameObject InventoryBar;
 
+    //Define MainWeapons
+    public GameObject M4;
+    public GameObject M200;
+    public GameObject MP5;
+    public GameObject PKM;
+
+    //Define SecondaryWeapons
+    public GameObject Knife;
+    public GameObject M9;
+    public GameObject RPG7;
+    public GameObject SPAS12;
+
+    //Define Throwables
+    public GameObject F1;
+
     //Weapon name
     private string MainWeaponName;
     private string SecondaryWeaponName;
@@ -31,7 +46,18 @@ public class Inventory : MonoBehaviour
     // Start is called before the first frame update
     void Start()
       {
-        //maybe disable movement that the user can select the weapons in "peace"
+        foreach (string weapon in MainWeapons)
+        {
+            GameObject.Find(weapon).SetActive(false);
+        }
+        foreach (string weapon in SecondaryWeapon)
+        {
+            GameObject.Find(weapon).SetActive(false);
+        }
+        foreach (string weapon in Throwables)
+        {
+            GameObject.Find(weapon).SetActive(false);
+        }
 
         //add listener on the button
         Button btn = BeginButton.GetComponent<Button>();
@@ -64,11 +90,60 @@ public class Inventory : MonoBehaviour
 
         //Hide the big weapon selector and show the smaller inventory
         HideUI();
+
+        ActivateWeapon();
+
     }
 
     void HideUI()
     {
         WeaponSelector.SetActive(false);
+    }
+
+    void ActivateWeapon()
+    {
+        //Activate Main Weapon
+        if(MainWeaponName == "M4")
+        {
+            M4.SetActive(true);
+        }
+
+        else if(MainWeaponName == "M200")
+        {
+            M200.SetActive(true);
+        }
+
+        else if(MainWeaponName == "MP5")
+        {
+            MP5.SetActive(true);
+        }
+
+        else if(MainWeaponName == "PKM")
+        {
+            PKM.SetActive(true);
+        }
+
+        //Activate Secondary Weapon
+        if (SecondaryWeaponName == "Knife")
+        {
+            Knife.SetActive(true);
+        }
+
+        else if (SecondaryWeaponName == "RPG7")
+        {
+            RPG7.SetActive(true);
+        }
+
+        else if (SecondaryWeaponName == "SPAS12")
+        {
+            SPAS12.SetActive(true);
+        }
+
+        //Activate Throwable
+        if(ThrowableName == "Grenade")
+        {
+            F1.SetActive(true);
+        }
     }
 
 
