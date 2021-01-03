e0d09d1d7840cff0ce9da0c0169e764da03cefc7
ynkf <yannickf@bluewin.ch>
2021-01-03 18:39:03 +0100

    created ui and server logic to create and join a room

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -1,14 +1,66 @@
 ﻿using Photon.Pun;
+using TMPro;
+using UnityEngine;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
+    [SerializeField]
+    private TMP_InputField _roomNameInputField;
+    [SerializeField]
+    private TMP_Text _errorText;
+    [SerializeField]
+    private TMP_Text _roomNameText;
+
     private void Start()
     {
         PhotonNetwork.ConnectUsingSettings();
     }
 
-    private void Update()
+    public override void OnConnectedToMaster()
     {
-        
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        MenuManager.Instance.OpenMenu(MenuNames.TitleMenu);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        MenuManager.Instance.OpenMenu(MenuNames.RoomMenu);
+        _roomNameText.text = PhotonNetwork.CurrentRoom.Name;
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        _errorText.text = "Room Creation Failed: " + message;
+        MenuManager.Instance.OpenMenu(MenuNames.ErrorMenu);
+    }
+
+    public override void OnLeftRoom()
+    {
+        MenuManager.Instance.OpenMenu(MenuNames.TitleMenu);
+    }
+
+    public void CreateRoom()
+    {
+        var roomName = _roomNameInputField.text;
+
+        if (string.IsNullOrEmpty(roomName))
+        {
+            return;
+        }
+
+        PhotonNetwork.CreateRoom(roomName);
+
+        MenuManager.Instance.OpenMenu(MenuNames.LoadingMenu);
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+
+        MenuManager.Instance.OpenMenu(MenuNames.LoadingMenu);
     }
 }
---
+++ Assets/Scripts/Menu/MenuNames.cs
@@ -0,0 +1,8 @@
+﻿public static class MenuNames
+{
+    public const string TitleMenu = "title";
+    public const string LoadingMenu = "loading";
+    public const string CreateRoomMenu = "createRoom";
+    public const string RoomMenu = "room";
+    public const string ErrorMenu = "error";
+}
