19e4b5ca26dbaf6e113f98a0a9efa71913ff9408
ynkf <yannickf@bluewin.ch>
2021-01-03 15:44:04 +0100

    imported PUN

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,14 @@
+﻿using Photon.Pun;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    private void Update()
+    {
+        
+    }
+}
