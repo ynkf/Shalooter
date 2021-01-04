fdbe708f415ec36e957d1e682be7f5fbf3b0be04
michelleisibach <50678154+michelleisibach@users.noreply.github.com>
2021-01-04 21:20:12 +0100

    created script for shooting

---
+++ Assets/Scripts/Weapons/WeaponScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponScript : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera Cam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Fire()
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(Cam.transform.position, Cam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+            //call damage on player
+        }
+    }
+}
